b2e940ee1a78dea46a3fc6c22d341ab78b836a53
javo787 <user.dr04@gmail.com>
2026-05-10 15:11:13 +0500

    Update dotenv config to use default path

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: '.env.local' });
+require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
 const http = require('http');
 const connectDB = require('./db/connection');
