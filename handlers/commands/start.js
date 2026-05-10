779b61df626fe6d529d2c3e0c92f36863d097f11
javo787 <user.dr04@gmail.com>
2026-05-10 12:31:37 +0500

    Include mainKeyboard in /start command
    Added mainKeyboard to the start command response.

--- handlers/commands/start.js
+++ handlers/commands/start.js
@@ -1,4 +1,5 @@
-const { safeSend, isAllowed } = require('../../utils/telegram');
+// handlers/commands/start.js
+const { safeSend, isAllowed, mainKeyboard } = require('../../utils/telegram'); // ← добавить mainKeyboard
 const { CONFIG } = require('../../config');
 
 module.exports = (bot) => {
@@ -7,8 +8,8 @@ module.exports = (bot) => {
     if (!isAllowed(tgId, CONFIG.userMap)) return safeSend(bot, tgId, '⛔ Кечирасиз, бу шахсий бот.');
     const name = msg.from.first_name || 'Доктор';
     await safeSend(bot, tgId,
-      `🏥 *Medical Brother v6.0*\n\nХуш келибсиз, *${name}!* 👋\n\n...`,
-      { reply_markup: mainKeyboard }
+      `🏥 *Medical Brother v6.0*\n\nХуш келибсиз, *${name}!* 👋`,
+      mainKeyboard  // ← правильная передача
     );
   });
 };
